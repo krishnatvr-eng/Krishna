c9e3d11ccae4a090ec12b276d5a487b3f8979e54
krishnatvr-eng <krishnatvr44@gmail.com>
2026-04-08 12:11:35 +0530

    ShellNavigation,Project1

---
+++ ShellNavigation/NewFolder/NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShellNavigation.NewFolder;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ShellNavigation/Pages/Page1.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShellNavigation.Pages;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
